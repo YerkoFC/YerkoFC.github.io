ad33f3024a7aecf62785181af6f3eb0e8b0e4d7e
YerkoFC <yerko.fonceac@utem.cl>
2020-05-19 22:05:44 -0300

    Add ScrollReveal

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log('funcionando...');
+
+ScrollReveal().reveal('.section-jobs', {delay: 300});
+ScrollReveal().reveal('.section-languages', {delay: 300});
+ScrollReveal().reveal('.section-frameworks', {delay: 300});
+ScrollReveal().reveal('.footer', {delay: 300});
